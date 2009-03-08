77ce72f56ff3fc486a767b6bdf617cf7841ace25
Alex Malinovich <alex.malinovich@planetargon.com>
2009-03-07 18:10:07 -0800

    Updating start-synergy to automatically start if necessary and wake up client

--- start-synergy.sh
+++ start-synergy.sh
@@ -11,7 +11,6 @@ showHelp () {
 	echo "See the synergyc or synergys help output for a list of levels."
 	echo "(ERROR by default, INFO along with the -n flag is useful for debugging."
 	echo ""
-	echo "  -r	Wake up the remote client by doing a dns cache flush"
 	echo "  -n	Don't start synergy in the background"
 	echo "  -h	Show this screen"
 	echo ""
@@ -20,9 +19,8 @@ showHelp () {
 
 
 x=1 # Avoids an error if we get no options at all.
-while getopts "rnh" opt; do
+while getopts "nh" opt; do
   case "$opt" in
-    r) REMOTE=1;;
     n) DETACH="";;
     h) showHelp;;
   esac
@@ -36,17 +34,13 @@ else
 	LEVEL="$1"
 fi
 
-if [ "$REMOTE" ]; then
-#	ssh `if [ -z "$DETACH" ]; then echo '-t'; fi` synergy "screen $DETACH /Applications/QuickSynergy.app/Contents/Resources/synergyc -f -d $LEVEL Shrike.argon.lan"
-	ssh synergy 'dscacheutil -flushcache'
-else
-	if ps -Af |grep -q [s]ynergys; then
-		echo "Synergy already running, not starting again."
-		exit 0
+if ps -Af |grep -q [s]ynergys; then
+	echo "Synergy already running, waking up client."
 #	screen $DETACH /Applications/QuickSynergy.app/Contents/Resources/synergys -f -d $LEVEL
-	elif [ "$DETACH" ]; then
-		synergys -f -d $LEVEL &
-	else
-		synergys -f -d $LEVEL
-	fi
+elif [ "$DETACH" ]; then
+	synergys -f -d $LEVEL &
+else
+	synergys -f -d $LEVEL
 fi
+
+ssh synergy 'dscacheutil -flushcache'
